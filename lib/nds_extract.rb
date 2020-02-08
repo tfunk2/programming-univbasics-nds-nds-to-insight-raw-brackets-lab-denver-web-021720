56a09efd6aa6ef6572e123d3e3d8379664af0019
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-08 04:46:42 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,20 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  
+  
+  director_index = 0
+  
+  while director_index < nds.length do
+    movie_index = 0
+    money_total = 0
+    
+    while movie_index < nds[director_index][:movies].length do
+      money_total += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+    result[(nds[director_index][:name])] = money_total
+    director_index += 1
+  end
+  result
 end
