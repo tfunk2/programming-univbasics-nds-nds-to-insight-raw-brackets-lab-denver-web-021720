7b69d16dbe9cf12aa007b956928ddd2f051a67a5
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-08 04:30:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -30,7 +30,7 @@ def directors_totals(nds)
       money_total += nds[director_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
     end
-    result << nds[director_index][:name]
+    result[:nds[director_index][:name]] = money_total
   end
   director_index += 1
   result
